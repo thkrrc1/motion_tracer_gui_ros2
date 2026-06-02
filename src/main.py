af6a1d44e1ec003611c1a6bfb926586ad6dad3ec
n-wada0828 <orihuran8828@gmail.com>
2026-06-02 19:42:00 +0900

    tracer起動ボタン＆停止ボタン 暫定実装

--- src/main.py
+++ src/main.py
@@ -12,6 +12,10 @@ from sensor_msgs.msg import CompressedImage as RosImage
 import numpy as np
 from rclpy.qos import qos_profile_sensor_data
 
+import subprocess
+import os
+import time
+
 class ImageSubscriber(Node):
     def __init__(self):
         super().__init__('image_subscriber')
@@ -63,6 +67,8 @@ class ImageGUI(ctk.CTk):
         super().__init__()
 
         self.node = ros_node
+
+        self.tracer_process = None
 
         self.title("MotionTracerGUI ROS2")
         self.geometry("1920x1080")
@@ -119,46 +125,54 @@ class ImageGUI(ctk.CTk):
 
         #-----------------------------------------------------------------------------------------
 
-        self.head_view_frame = ctk.CTkFrame(self.main_frame)
-        self.head_view_frame.pack(side=ctk.TOP)
-
-        head_image_frame = ctk.CTkFrame(self.head_view_frame)
+        head_image_frame = ctk.CTkFrame(self.main_frame)
         head_image_frame.grid(row=0, column=0)
 
-        head_label = ctk.CTkLabel(head_image_frame, text="/camera1/image_raw/compressed\nWaiting for image...")
+        head_label = ctk.CTkLabel(head_image_frame, width=960, height=540, text="/camera1/image_raw/compressed\nWaiting for image...")
         head_label.pack(padx=1)
         self.labels["camera1"] = head_label
 
-        self.arm_view_frame = ctk.CTkFrame(self.main_frame)
-        self.arm_view_frame.pack(side=ctk.TOP)
+        right_image_frame = ctk.CTkFrame(self.main_frame)
+        right_image_frame.grid(row=0, column=1)
 
-        left_image_frame = ctk.CTkFrame(self.head_view_frame)
-        left_image_frame.grid(row=0, column=1)
-
-        left_label = ctk.CTkLabel(left_image_frame, text="/camera2/image_raw/compressed\nWaiting for image...")
-        left_label.pack(padx=1)
-        self.labels["camera2"] = left_label
-
-        right_image_frame = ctk.CTkFrame(self.head_view_frame)
-        right_image_frame.grid(row=1, column=0)
-
-        right_label = ctk.CTkLabel(right_image_frame, text="/camera3/image_raw/compressed\nWaiting for image...")
+        right_label = ctk.CTkLabel(right_image_frame, width=960, height=540, text="/camera2/image_raw/compressed\nWaiting for image...")
         right_label.pack(padx=1)
-        self.labels["camera3"] = right_label
+        self.labels["camera2"] = right_label
+
+        left_image_frame = ctk.CTkFrame(self.main_frame)
+        left_image_frame.grid(row=1, column=0)
+
+        left_label = ctk.CTkLabel(left_image_frame, width=960, height=540, text="/camera3/image_raw/compressed\nWaiting for image...")
+        left_label.pack(padx=1)
+        self.labels["camera3"] = left_label
 
         #-----------------------------------------------------------------------------------------
 
-        control_frame = ctk.CTkFrame(self.main_frame)
-        control_frame.pack(side=ctk.LEFT)
+        control_frame = ctk.CTkFrame(self.main_frame, width=960, height=540)
+        control_frame.grid(row=1, column=1)
+        # control_frame.grid_propagate(False)
 
-        robot_bringup_button = ctk.CTkButton(control_frame, text="Robot Bring Up", width=200, height=200, command=self.on_robot_bringup_click)
-        robot_bringup_button.grid(row=0, column=0, padx=10)
+        control_button_frame = ctk.CTkFrame(control_frame, width=960, height=270)
+        control_button_frame.pack(side=ctk.TOP, pady=(0, 50))
+        # control_button_frame.pack_propagate(False)
 
-        tracer_bringup_button = ctk.CTkButton(control_frame, text="Tracer Bring Up", width=200, height=200, command=self.on_tracer_bringup_click)
-        tracer_bringup_button.grid(row=0, column=1, padx=10)
+        robot_bringup_button = ctk.CTkButton(control_button_frame, text="Robot Bring Up", width=150, height=150, command=self.on_robot_bringup_click)
+        robot_bringup_button.grid(row=0, column=0, padx=10, pady=0)
 
-        finish_button = ctk.CTkButton(control_frame, text="All Finish", width=200, height=200, command=self.on_finish_click)
-        finish_button.grid(row=0, column=2, padx=10)
+        tracer_bringup_button = ctk.CTkButton(control_button_frame, text="Tracer Bring Up", width=150, height=150, command=self.on_tracer_bringup_click)
+        tracer_bringup_button.grid(row=0, column=1, padx=10, pady=0)
+
+        finish_button = ctk.CTkButton(control_button_frame, text="All Finish", width=150, height=150, command=self.on_finish_click)
+        finish_button.grid(row=0, column=2, padx=10, pady=0)
+
+        control_slider_frame = ctk.CTkFrame(control_frame, width=960, height=270)
+        control_slider_frame.pack(side=ctk.BOTTOM)
+
+        grip_current_slider = ctk.CTkSlider(control_slider_frame, from_=1, to=100, number_of_steps=99, command=self.on_slider_change)
+        grip_current_slider.set(30)
+        grip_current_slider.pack()
+
+        self.after(1000, lambda: print(control_frame.winfo_height()))
 
         self.update_images()
 
@@ -166,22 +180,84 @@ class ImageGUI(ctk.CTk):
     
     def on_robot_bringup_click(self):
         print("ロボット起動ボタンがクリックされました")
+        threading.Thread(
+            target=self.start_robot_bringup,
+            daemon=True
+        ).start()
+
+    def start_robot_bringup(self):
+        print("ロボット起動しました")
 
     def on_tracer_bringup_click(self):
         print("トレーサー起動ボタンがクリックされました")
 
+        threading.Thread(
+            target=self.start_tracer_bringup,
+            daemon=True
+        ).start()
+
+    def start_tracer_bringup(self):
+        print("トレーサー起動しました")
+
+        if self.tracer_process is not None:
+            print("Tracerは既に起動中です")
+            return
+
+        self.tracer_process = subprocess.Popen(
+            [
+                "gnome-terminal",
+                "--",
+                "bash",
+                "-lc",
+                "source /opt/ros/jazzy/setup.bash && "
+                "source /home/seed/ros2_ws/install/setup.bash && "
+                "ros2 launch motion_tracer_ros2 tracer_bringup.launch.py"
+            ],
+            preexec_fn=os.setsid
+        )
+
     def on_finish_click(self):
         print("終了ボタンがクリックされました")
+
+        threading.Thread(
+            target=self.finish_all,
+            daemon=True
+        ).start()
+
+    def finish_all(self):
+        print("全システム停止")
+
+        subprocess.run(
+            [
+                "pkill",
+                "-SIGINT",
+                "-f",
+                "ros2"
+            ]
+        )
+        time.sleep(1)
+
+        if self.tracer_process is not None:
+            try:
+                self.tracer_process.terminate()
+                self.tracer_process.wait(timeout=3)
+            except subprocess.TimeoutExpired:
+                self.tracer_process.kill()
+            self.tracer_process = None
 
     def cv_to_tk(self, cv_img):
         rgb = cv2.cvtColor(cv_img, cv2.COLOR_BGR2RGB)
         # rgb = cv2.resize(rgb, (638, 800))
         # rgb = cv2.resize(rgb, (588, 440))
-        rgb = cv2.resize(rgb, (960, 512))
+        # rgb = cv2.resize(rgb, (960, 512))
+        rgb = cv2.resize(rgb, (960, 540))
 
         pil_img = Image.fromarray(rgb)
 
         return ImageTk.PhotoImage(pil_img)
+
+    def on_slider_change(self, value):
+        print("現在のスライダーの値:", round(value, 2))
 
     def update_images(self):
         with self.node.lock:
@@ -201,7 +277,6 @@ class ImageGUI(ctk.CTk):
 def ros_spin(node):
     rclpy.spin(node)
 
-
 def main():
     rclpy.init()
 
@@ -219,7 +294,6 @@ def main():
             rclpy.shutdown()
         spin_thread.join()
         node.destroy_node()
-
 
 if __name__ == '__main__':
     main()
